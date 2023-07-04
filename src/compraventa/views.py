8cfe4df58c312fb985e0f037418740ea0262a236
felo1 <felipe.bazignan@gmail.com>
2023-07-04 16:47:15 -0400

    separar vista pedido cliente y pedido admin

--- src/compraventa/views.py
+++ src/compraventa/views.py
@@ -7,13 +7,45 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from .forms import pedidos_manuales
+from .forms import pedidos_manuales, pedidos_manuales_cliente
 import datetime
 from django.contrib.auth.models import Group, User
 # Create your views here.
 
 def index(request):
     return render(request, 'compraventa/index.html')
+
+def pedido_manual_cliente(request):
+    form = pedidos_manuales_cliente()
+    if request.method == "POST":
+        print(request)
+        form = pedidos_manuales_cliente(request.POST)
+        if form.is_valid():
+            print(form)
+            pedido = Pedido()
+            try:
+                cliente = Cliente.objects.get(user=request.user)
+                pedido.cliente_solicitante = cliente
+            except Cliente.DoesNotExist:
+                raise ValueError("No se encuentra el cliente de origen")
+            pedido.productos = form.cleaned_data['productos']
+            if Pedido.objects.exists():
+                ultimo_pedido = Pedido.objects.latest('numero_transaccion')
+                pedido.numero_transaccion = ultimo_pedido.numero_transaccion + 1
+            else:
+                pedido.numero_transaccion = 1
+            pedido.subtotal = pedido.productos.precio
+            pedido.fecha_pedido = datetime.datetime.now()
+            pedido.save()
+            messages.success(request, 'Pedido ingresado exitosamente')
+        else:
+            print("Datos invalidos")
+        return redirect('pedido_manual_cliente')
+    context = {
+        'form': form
+    }
+    return render(request, 'compraventa/pedidos externos.html', context=context)
+
 
 def pedido_manual(request):
     form = pedidos_manuales()
@@ -29,7 +61,6 @@ def pedido_manual(request):
             pedido.productos = form.cleaned_data['productos']
             #pedido.numero_transaccion = #pull highest number in database + 1
             #pedido.subtotal = sum(producto.precio for producto in form.cleaned_data['productos'])
-            
             #-- estimar numero de orden
             #--falla si está vacío    
             if Pedido.objects.exists():
@@ -53,8 +84,6 @@ def registrar_usuario(request):
     if request.method == 'POST':
         form = RegistrarUsuarioForm(request.POST)
         print(form)
-        
-        
         if form.is_valid():
             
             user = form.save() #guardar formulario
