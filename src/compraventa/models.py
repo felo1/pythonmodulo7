47dafb96cf0484d00d4ac5372149fb96607e16e2
Felo <felipe.bazignan@gmail.com>
2023-07-04 22:35:25 -0400

    avances varios 4-7

--- src/compraventa/models.py
+++ src/compraventa/models.py
@@ -8,55 +8,56 @@ class Categoria(models.Model):
         return self.nombre
     
 #considerar categoria padre e hija, como implementar esto?
-
-class Proveedor(models.Model):
-    nombre = models.CharField(max_length=30)
-    def __str__(self):
-        return self.nombre
-
 class Producto(models.Model):
     nombre = models.CharField(max_length=30)
     precio = models.IntegerField(default=1)
-    sku = models.CharField(max_length=15)
-    stock = models.IntegerField(default=0) # Stock per sucursal
-    #proveedor = models.ForeignKey('Proveedor', blank=True)  # 
-    modelo = models.CharField(max_length=250, default=None) #deberá ser llenado de forma que se pueda parsear para presentar el contenido. 
+    sku = models.AutoField(primary_key=True)
+    stock = models.IntegerField(default=0)
+    modelo = models.CharField(max_length=250, default=None) 
+    #deberá ser llenado de forma que se pueda parsear para presentar el contenido. 
     #cual sería la mejor manera de lidiar con esto?
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, default=None)
-    impuesto = models.FloatField(default=0)
+    impuesto = models.FloatField(default=19, verbose_name="IVA")
     descuento = models.FloatField(default=0) # descuento base + descuentos circunstanciales
 
     def __str__(self):
-        return 'sku: ' + self.sku + ' | ' + self.nombre
+        return 'sku: ' + str(self.sku) + ' | ' + self.nombre
 
-#modificar la clase cliente para que sea una extensión del usuario base, para aprovechar las funcionalidades
-#que tendrá prontamente de login (para acceder a menu de transacciones históricas, despachos pendientes, etc), etc.
 class Cliente(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True) #uno a uno cliente con usuario, importante agregar primary key
-    #para que se vea en el admin, se modifica también el admin.py
-    nombres = models.CharField(max_length=30)
-    apellidos = models.CharField(max_length=30)
-    rut = models.CharField(max_length=11)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True) 
+    rut = models.CharField(max_length=11, unique=True)
+    nombres = models.CharField(max_length=15, default="", blank=True)
+    apellidos =  models.CharField(max_length=15, default="", blank=True)
+    telefono_fijo = models.CharField(max_length=15, default="", blank=True)
     telefono_movil = models.CharField(max_length=30, default="", blank=True)
-    telefono_fijo = models.CharField(max_length=30, default="", blank=True)  
     notas = models.CharField(max_length=250, default="", blank=True)
-    direcciones = models.CharField(max_length=250, default="", blank=True)
+    #direcciones = models.ManyToManyField(Direccion, max_length=100, default="", blank=True)
 
     def __str__(self):
         return self.nombres + ' ' + self.apellidos
-
+    
+class Direccion(models.Model):
+    direccion = models.CharField(max_length=250, default="")
+    usuario = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=None)
+    def __str__(self) -> str:
+        return self.direccion
+    
 class Pedido(models.Model):
+    #ver como restringiur modificar algunas cosas que en un producto de la vida real no deberian ser
+    #modificables en la administracion una vez generado un pedido.
     cliente_solicitante = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING, default=None)
-    productos = models.ForeignKey(Producto, on_delete=models.DO_NOTHING, default=None)
-    numero_transaccion = models.IntegerField(default=0) # n° de orden de compra
+    productos = models.ManyToManyField(Producto, default=None)
+    #ver alguna forma de almacenar numeros de boleta de fantasia que no intervengan con 
+    #el ingreso de los productos en los pedidos
+    #posiblemente con shortUID como mencionó anibal (para generar IDs publoicos aleatorios)
+    #si funcionan como index SIN ser el ID, que es algo de uso interno
+    numero_transaccion = models.AutoField(primary_key=True) # n° de orden de compra
     #para trazabilidad de la info.
-    subtotal = models.FloatField(default=0)
-    suma_descuentos = models.FloatField(default=0) # sumatoria de los descuentos individuales
-    total_pedido = models.FloatField(default=0) # monto a pagar, descuentos e intereses
-    fecha_pedido = models.DateTimeField(default='2023-1-1')
+    subtotal = models.FloatField(default=0, verbose_name="Subtotal")
+    suma_descuentos = models.FloatField(default=0, verbose_name="Descuentos") # sumatoria de los descuentos individuales
+    total_pedido = models.FloatField(default=0, verbose_name="Total a pagar") # monto a pagar, descuentos e intereses
+    fecha_pedido = models.DateTimeField()
     tiene_despacho = models.BooleanField
-    #TODO: estado despacho debe ser un selector
-
     ESTADO_CHOICES = [
         ('Recibido', 'Recibido'),
         ('Pago aceptado', 'Pago aceptado'),
@@ -64,13 +65,13 @@ class Pedido(models.Model):
         ('En proceso', 'En proceso'),
         ('Enviado', 'Enviado'),
         ('En transito', 'En transito'),
-        ('Despachado', 'Despachado'),
+        ('Entregado', 'Entregado'),
     ]
     estado_despacho = models.CharField(max_length=30, choices=ESTADO_CHOICES, default="Recibido", blank=False)
-    direccion = models.CharField(max_length=250, default="")
+    direccion_despacho = models.ForeignKey(Direccion, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return str(self.numero_transaccion)
+        return 'Orden de compra N°: ' + str(self.numero_transaccion) + ' | Nombre de cliente: '+ self.cliente_solicitante.nombres + ' ' + self.cliente_solicitante.apellidos + " | Estado de despacho: " + self.estado_despacho
     
 class Sucursal(models.Model):
     nombre = models.CharField(max_length=30)
